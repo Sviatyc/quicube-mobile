898e67f5174cc3aa9d2a1729d35a2fb184115a6c
Sviatoslav Nakonechnyi <dielok4@gmail.com>
2024-04-18 17:15:03 +0300

    build:Create quicube-mobile app

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  const [time, useTime] = useState(0)
+
+
+  return (
+    <View style={styles.container}>
+      <Text>{time}</Text>
+      <Button title='Click me' onPress={()=>useTime(x=>x+1)}/> 
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
