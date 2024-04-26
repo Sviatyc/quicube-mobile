a7b95112391c5e3cd5e85e8f86851f635a3bc1b2
Sviatoslav Nakonechnyi <dielok4@gmail.com>
2024-04-27 02:31:23 +0300

    ci:Add eas.json, change app src

---
+++ App.js
@@ -0,0 +1,10 @@
+import { SafeAreaView } from "react-native";
+import MainScreen from "./src/screens/Main/MainScreen.js";
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <MainScreen />
+    </SafeAreaView>
+  );
+}
